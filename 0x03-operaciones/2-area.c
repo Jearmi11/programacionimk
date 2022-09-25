67caef7cac55c6c79db4342f9ffa60f94430ad19
Jearmi11 <112347541+Jearmi11@users.noreply.github.com>
2022-09-24 20:11:19 -0500

    0x04-decisiones

--- 0x03-operaciones/2-area.c
+++ 0x03-operaciones/2-area.c
@@ -1,20 +1,6 @@
-#include <stdio.h>
+#include "main.h"
 
-int main()
+float area(float largo, float ancho)
 {
-    float eng, phy, chem, math, comp; 
-    float total, average, percentage;
-
-    printf("Enter marks of five subjects: \n");
-    scanf("%f%f%f%f%f", &eng, &phy, &chem, &math, &comp);
-
-    total = eng + phy + chem + math + comp;
-    average = total / 5.0;
-    percentage = (total / 500.0) * 100;
-
-    printf("Total marks = %.2f\n", total);
-    printf("Average marks = %.2f\n", average);
-    printf("Percentage = %.2f\n", percentage);
-
-    return 0;
+    return (largo * ancho);
 }
