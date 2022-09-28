e1764331b4969e355bf6f53644da645a3e577926
Jearmi11 <112347541+Jearmi11@users.noreply.github.com>
2022-09-28 13:13:17 -0500

    0x0A_examenParcial

--- 0x0A_examenParcial/area.c
+++ 0x0A_examenParcial/area.c
@@ -7,7 +7,7 @@ int area(float volumen, float densidad, float costoAnterior)
 {
     // declaro variables//
     float area, radio, y;
-    float gramos;
+    float gr;
     float pi = 3.1416;
     float costo, costm2 = 2.45;
 
@@ -16,21 +16,20 @@ int area(float volumen, float densidad, float costoAnterior)
 
     area = (2*pi*(pow(radio,2)) + 2*pi*radio*(volumen/(pi*pow(radio,2))));
 
-    y = (volumen/(pi*pow(radio,2)));
 
     costo = area * costm2;
     costo = costo/1000;
 
-    gramos = volumen * densidad;
+    gr = volumen * densidad;
     float ahorro = costoAnterior - costo;
 
     // imprime en pantalla //
 
-    printf("Area total de la lata: %0.2f cm2\n", area);   
+    printf("Area de la lata: %0.2f cm2\n", area);   
     printf("Costo de fabricacion: $ %0.2f\n", costo);     
-    printf("Costo anterior para lata de %0.f ml: $ %0.2f\n", volumen, costoAnterior);
-    printf("Ahorro: $ %0.2f\n", ahorro);
-    printf("%0.f ml en gramos = %0.2f gr\n",volumen,gramos);
+    printf("Costo anterior de fabricación de una lata de %0.f ml: $ %0.2f\n", volumen, costoAnterior);
+    printf("Se ahorro: $ %0.2f\n", ahorro);
+    printf("%0.f ml en gramos = %0.2f gr\n",volumen,gr);
 
 }
 
